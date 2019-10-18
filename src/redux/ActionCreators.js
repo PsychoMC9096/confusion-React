f8620972e979db6f27896809a15770ffecf8f782
PsychoMC9096 <jordanmeredith9096@gmail.com>
2019-10-18 04:16:36 -0700

    React Animation Components

--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -1,16 +1,6 @@
 import * as ActionTypes from './ActionTypes';
 import { baseUrl } from '../shared/baseUrl';
 
-export const addComment = (dishId, rating, author, comment) => ({
-    type: ActionTypes.ADD_COMMENT,
-    payload: {
-        dishId: dishId,
-        rating: rating,
-        author: author,
-        comment: comment
-    }
-});
-
 //DISHES ACTIONS
 export const fetchDishes = () => (dispatch) => {
 
